de602edc1c06b461794663ef517d827c946cfeec
İbrahim Gün <ibrahimggun@gmail.com>
2021-08-25 18:26:13 +0300

    initial commit

---
+++ Project9/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by İbrahim Gün on 19.08.2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: String
+}
