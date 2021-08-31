514db3509cc8c00f6ae807a2c039e10c26597f37
İbrahim Gün <ibrahimggun@gmail.com>
2021-09-01 00:08:43 +0300

    initial

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by İbrahim Gün on 26.08.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
