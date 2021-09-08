6c77c584dd3aed32f57a0a839875fd422edc445c
İbrahim Gün <ibrahimggun@gmail.com>
2021-09-08 22:58:04 +0300

    initial commit

---
+++ Project16/Project16/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by İbrahim Gün on 8.09.2021.
+//
+import MapKit
+import UIKit
+
+class ViewController: UIViewController,MKMapViewDelegate {
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.50722, longitude: -0.1275), info: "Home to the 2012 Summer Olympics")
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
+        let paris = Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3506), info: "Often called the City of Light")
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+        let ankara = Capital(title: "Ankara", coordinate: CLLocationCoordinate2D(latitude: 39.9334, longitude: 32.8597), info: "Ankara'nin baglari buklum buklum yollari")
+        
+        mapView.addAnnotation(london)
+        mapView.addAnnotation(oslo)
+        mapView.addAnnotation(paris)
+        mapView.addAnnotation(rome)
+        mapView.addAnnotation(ankara)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard annotation is Capital else { return nil }
+        
+        let identifier = "Capital"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+            
+            let btn = UIButton(type: .detailDisclosure)
+            annotationView?.rightCalloutAccessoryView = btn
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        return  annotationView
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let capital = view.annotation as? Capital else { return }
+        
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+}
+
